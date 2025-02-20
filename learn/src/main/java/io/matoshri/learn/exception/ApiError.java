484c334df042dab974769a0ab68b4d0f577b14b2
prakash-mac <prakash8876@gmail.com>
2025-02-21 05:02:25 +0530

    handling exception

--- learn/src/main/java/io/matoshri/learn/exception/ApiError.java
+++ learn/src/main/java/io/matoshri/learn/exception/ApiError.java
@@ -1,2 +1,6 @@
-package io.matoshri.learn.exception;public class ApiError {
+package io.matoshri.learn.exception;
+
+import java.time.LocalDateTime;
+
+public record ApiError(String path, String message, int httpStatusCode, LocalDateTime timestamp) {
 }
