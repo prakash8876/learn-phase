02b353b7253e7017acf746d7a653bed4bc909ac0
prakash-mac <prakash8876@gmail.com>
2025-02-22 02:51:47 +0530

    Some configuration changes

--- learn/src/main/java/io/matoshri/learn/exception/ApiError.java
+++ learn/src/main/java/io/matoshri/learn/exception/ApiError.java
@@ -1,9 +1,6 @@
 package io.matoshri.learn.exception;
 
-import org.springframework.stereotype.Component;
-
 import java.time.LocalDateTime;
 
-@Component
 public record ApiError(String path, String message, int httpStatusCode, LocalDateTime timestamp) {
 }
--- learn/src/main/java/io/matoshri/learn/exception/StudentException.java
+++ learn/src/main/java/io/matoshri/learn/exception/StudentException.java
@@ -1,12 +1,7 @@
 package io.matoshri.learn.exception;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class StudentException extends AppRuntimeException {
 
-    @Autowired
     public StudentException() {
         super();
     }
