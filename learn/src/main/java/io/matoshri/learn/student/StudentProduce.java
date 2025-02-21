ff764faf05745a9c7989e368356c572af1f3f7df
prakash-mac <prakash8876@gmail.com>
2025-02-22 02:52:16 +0530

    Some configuration changes

--- learn/src/main/java/io/matoshri/learn/student/StudentProduce.java
+++ learn/src/main/java/io/matoshri/learn/student/StudentProduce.java
@@ -1,8 +1,5 @@
 package io.matoshri.learn.student;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public record StudentProduce(
         Integer studentId,
         String studentName,
