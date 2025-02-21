297751be7ba65597a0ff1457c2765a9cd09c7025
Prakash <prakash8876@gmail.com>
2025-02-22 03:13:16 +0530

    Update data.sql

--- learn/src/main/resources/data.sql
+++ learn/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO address (area,city,pin_code,id) values ('GANDHI NAGAR','PUNE','411011',default)
-INSERT INTO college (address_id,college_name,id) values (1,'KIDZEE',default)
+INSERT INTO address (area,city,pin_code,id) values ('GANDHI NAGAR','PUNE','411011',default);
+INSERT INTO college (address_id,college_name,id) values (1,'KIDZEE',default);
