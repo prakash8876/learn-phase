fc7244c950501db60e7b32aa95b4d61d182b492d
Prakash <prakash8876@gmail.com>
2025-03-02 03:11:53 +0530

    #6 feat: implemented docker compose

--- learn/src/main/resources/data.sql
+++ learn/src/main/resources/data.sql
@@ -1,33 +1,9 @@
-INSERT INTO
-  address (id, area, city, pin_code)
-values
-  (DEFAULT, 'GANDHI NAGAR', 'PUNE', '411011'),
-  (DEFAULT, 'ABC Road', 'Delhi', '011011'),
-  (DEFAULT, 'ACD Lane 2', 'Delhi', '611011');
+INSERT INTO  address (area, city, pin_code) values ('GANDHI NAGAR', 'PUNE', '411011'),
+                                                    ('ABC Road', 'Delhi', '011011'),
+                                                    ('ACD Lane 2', 'Delhi', '611011');
 
-INSERT INTO
-  college (address_id, id, college_name)
-values
-  (1, DEFAULT, 'KIDZEE'),
-  (2, DEFAULT, 'IIT');
+INSERT INTO  college (address_id,college_name) values (1,'KIDZEE'),(2, 'IIT');
 
-INSERT INTO
-  student (
-    id,
-    student_name,
-    student_email,
-    student_class,
-    college_id,
-    address_id,
-    created_at
-  )
-values
-  (
-    DEFAULT,
-    'Rutu',
-    'rutu@email.com',
-    'PlayGroup',
-    2,
-    3,
-    CURRENT_TIMESTAMP()
-  );
+INSERT INTO  student (student_name,student_email,student_class,college_id,address_id,created_at)values ('Rutu','rutu@email.com','PlayGroup',1,2,CURRENT_TIMESTAMP);
+
+COMMIT;
